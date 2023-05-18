71b4eb44c3e22485efc755ea42647e066060cd7b
ArshadMohammadTCD <mohammar@tcd.ie>
2023-05-18 17:38:39 +0100

    Added a cool card effect

---
+++ app/app.js
@@ -0,0 +1,5 @@
+const letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+document.querySelector("h1").onmouseover = event => {
+    event.target.innerText = event.target.innerText.split("")
+    .map(letter => letters[Math.floor(Math.random() * 26)]).join();
+}
